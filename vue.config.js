a851dbb557218adebaa59778d06127c36027d613
cglx007 <xiangqi_007@163.com>
2023-12-17 16:45:30 +0800

    vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,46 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path');
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  publicPath: process.env.NODE_ENV === 'production' ? '' : '/',
+  outputDir: process.env.NODE_ENV === 'production' ? 'dist' : 'devdist',
+  lintOnSave: true,
+  chainWebpack: (config) => {
+  },
+  configureWebpack: (config) => {
+    config.resolve = { // 配置解析别名
+      extensions: ['.js', '.json', '.vue'], // 自动添加文件名后缀
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        'public': path.resolve(__dirname, './public'),
+        '@c': path.resolve(__dirname, './src/components'),
+        'common': path.resolve(__dirname, './src/common'),
+        'api': path.resolve(__dirname, './src/api'),
+        'views': path.resolve(__dirname, './src/views'),
+        'data': path.resolve(__dirname, './src/data')
+      }
+    }
+  },
+  productionSourceMap: false,
+  css: {
+    extract: true,
+    sourceMap: false,
+    loaderOptions: {
+      scss: {
+        additionalData: `@import "src/styles/main.scss";`
+      }
+    },
+  },
+  parallel: require('os').cpus().length > 1,
+  pwa: {},
+  devServer: {
+    open: false, // 编译完成是否打开网页
+    host: '0.0.0.0', // 指定使用地址，默认localhost,0.0.0.0代表可以被外界访问
+    port: 8080, // 访问端口
+    https: false, // 编译失败时刷新页面
+    hot: true, // 开启热加载
+    proxy: null, // 设置代理
+  },
+  pluginOptions: {}
 })
